95fcd727c11fa6107672a1dbeeb24aa00eddcd77
Adam Kiss <iam@adamkiss.com>
2022-09-07 18:37:06 +0200

    Create build.php

---
+++ build.php
@@ -0,0 +1,19 @@
+<?php
+
+define('KIRBY_HELPER_DUMP', false);
+define('KIRBY_HELPER_GO', false);
+
+require 'kirby/bootstrap.php';
+
+$kirby = new Kirby();
+
+$staticSiteGenerator = new D4L\StaticSiteGenerator($kirby, [], null);
+$staticSiteGenerator->setCustomRoutes([
+	[
+		'path' => '404.html',
+		'page' => page('error')
+	],
+]);
+$fileList = $staticSiteGenerator->generate($outputFolder = '__build', $baseUrl = '/', $preserve = []);
+
+echo A::join($fileList, "\n");
